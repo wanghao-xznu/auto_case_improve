5873a56998da4fd8da2bf2ad3c46bf0fc2bc6aa2
wanghao <wanghao.xznu@gmail.com>
2014-05-30 14:07:55 +0800

    add .gitignore and before change compare.sh and add the TGE-LV-DEFAULT-CASE for magic.sh generate

--- compare.sh
+++ compare.sh
@@ -5,22 +5,26 @@ current_rootfs=`/mnt/nfs/tools/printenv nfsroot`
 #case_kernel=`cat auto_case_table.txt | grep "TGE-LV-NAND-1101" | awk '{print $2}'`
 #case_fdt=`cat auto_case_table.txt | grep "TGE-LV-NAND-1101" | awk '{print $3}'`
 #case_rootfs=`cat auto_case_table.txt | grep "TGE-LV-NAND-1101" | awk '{print $4}'`
+ct=0
 
 case_kernel=`cat /mnt/nfs/tools/auto_case_table.txt | grep "$1" | awk '{print $2}'`; echo case_kernel=$case_kernel
 case_fdt=`cat /mnt/nfs/tools/auto_case_table.txt | grep "$1" | awk '{print $3}'`   ; echo case_fdt=$case_fdt
 case_rootfs=`cat /mnt/nfs/tools/auto_case_table.txt | grep "$1" | awk '{print $4}'`; echo case_rootf=s$case_rootfs
 
-ct=0
+
 ##############################in case there is no case id in table sheet##########################################
 case_id=`cat /mnt/nfs/tools/auto_case_table.txt | grep "$1" | awk '{print $1}'`
 if test -z "$case_id"
 then
-    exit 0
+    ############here need use default kernel dtb rootfs##############
+    #exit -1                   ####can't return zero,because ltp-pan check the exit vaule and have a reboot#####
+    case_kernel=`cat /mnt/nfs/tools/auto_case_table.txt | grep "TGE-LV-DEFAULT-CASE" | awk '{print $2}'`; echo case_kernel=$case_kernel
+    case_fdt=`cat /mnt/nfs/tools/auto_case_table.txt | grep "TGE-LV-DEFAULT-CASE" | awk '{print $3}'`   ; echo case_fdt=$case_fdt
+    case_rootfs=`cat /mnt/nfs/tools/auto_case_table.txt | grep "TGE-LV-DEFAULT-CASE" | awk '{print $4}'`; echo case_rootf=s$case_rootfs
 else
     echo "case_id=$case_id"
 fi
 ##############################in case there is no case id in table sheet##########################################
-
 
 
 if [ "$current_kernel" != "$case_kernel" ]; then
--- magic.sh
+++ magic.sh
@@ -13,8 +13,6 @@ if [ $# -eq 0 ];then
     exit 1
 fi
 
-
-
 release_id=$1
 platform=$2
 #replace_list=uImage ecspi gpmi_weim rootfs_r_fb rootfs_ra_wld rootfs_r_dfb 
@@ -26,6 +24,7 @@ platform=$2
 #    cat temp_table.txt | sed 's/'"$i"'/'"uImage_$2_$1"'/g' | tee temp_table.txt
 #    sleep 1
 #done
+
     cat generate_table.txt | sed 's/uImage/'"uImage_$2_$1"'/g' | tee temp_table.txt
     sleep 1
     cat temp_table.txt | sed 's/ecspi/'"dtb_$2$3_ecspi_$1"'/g' | tee temp_table.txt
@@ -38,4 +37,12 @@ platform=$2
     sleep 1
     cat temp_table.txt | sed 's/rootfs_r_wld/'"\/rootfs\/i$2_rootfs_r_wld"'/g' | tee temp_table.txt
     sleep 1
+    cat temp_table.txt | sed 's/rootfs_r/'"\/rootfs\/i$2_rootfs_r"'/g' | tee temp_table.txt
+    sleep 1
+#############last define the default###################
+    echo "TGE-LV-DEFAULT-CASE uImage_$2_$1 dtb_$2$3_$1 /rootfs/i$2_rootfs_r" >> temp_table.txt
+    cp temp_table.txt auto_case_table.txt
+
+
+
     exit 0
